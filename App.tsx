10397aef88e35792bc682b082226f52c422daff7
Bruno Rocha <bruno.rocha.dev@outlook.com>
2023-03-18 00:26:35 -0300

    Hide splash screen order

--- App.tsx
+++ App.tsx
@@ -4,31 +4,15 @@ import { Main } from "./src";
 import { ThemeProvider } from "./src/themes/provider/ThemeProvider";
 import { StatusBar } from "expo-status-bar";
 import * as SplashScreen from "expo-splash-screen";
-import { useFonts } from "expo-font";
 import { AuthProvider } from "./src/context/AuthContext";
 
 export default function App() {
-  const [fontsLoaded] = useFonts({
-    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
-    "Poppins-Medium": require("./assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-SemiBold": require("./assets/fonts/Poppins-SemiBold.ttf"),
-    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
-  });
-
   useEffect(() => {
     async function prepare() {
       await SplashScreen.preventAutoHideAsync();
     }
     prepare();
   }, []);
-
-  if (!fontsLoaded) {
-    return null;
-  }
-
-  if (fontsLoaded) {
-    SplashScreen.hideAsync();
-  }
 
   return (
     <AuthProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,26 @@
+import { useFonts } from "expo-font";
 import React, { useContext } from "react";
-import { LoadingScreen } from "./components/LoadingScreen";
 import { AuthContext } from "./context/AuthContext";
 import { PublicRoutes } from "./routes/public.routes";
+import * as SplashScreen from "expo-splash-screen";
 
 export const Main = () => {
   const { signed, loading } = useContext(AuthContext);
 
-  if (loading) {
-    return <LoadingScreen />;
+  const [fontsLoaded] = useFonts({
+    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "Poppins-SemiBold": require("../assets/fonts/Poppins-SemiBold.ttf"),
+    "Poppins-Bold": require("../assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  if (loading && !fontsLoaded) {
+    return null;
   }
 
-  return signed ? <PublicRoutes /> : null;
+  if (fontsLoaded && !loading) {
+    SplashScreen.hideAsync();
+  }
+
+  return signed ? <PublicRoutes /> : <PublicRoutes />;
 };
